c0a433fe5db246149e540762f6a943d7e70d9fd0
pyatnicyn.stanislav <pyatnicyn.stanislav@yandex.ru>
2020-08-08 23:28:47 +0300

    Added ChipGroup in BookFragment

--- app/src/main/java/com/spappstudio/myapplication/MainActivity.java
+++ app/src/main/java/com/spappstudio/myapplication/MainActivity.java
@@ -14,9 +14,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.chip.Chip;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
@@ -71,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main_2);
+
+        //chip_current.setSelected(true);
 
         sharedPreferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         dbHelper = new DBHelper(this);
@@ -319,6 +323,18 @@ public class MainActivity extends AppCompatActivity {
         imageButtonBarMonth.setBackgroundResource(R.drawable.rounded_corners_4);
         imageButtonLineMonth.setBackgroundColor(Color.WHITE);
         createGraphMonth();
+    }
+
+    public void onClickButtonCurrent(View view) {
+
+    }
+
+    public void onClickButtonArchive(View view) {
+
+    }
+
+    public void onClickButtonWishful(View view) {
+
     }
 
     public void createGraph() {
